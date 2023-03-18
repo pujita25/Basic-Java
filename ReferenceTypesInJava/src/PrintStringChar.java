30bb2ceaacb2f9bd41fe5c4284b3ae28ee964491
Pujita Bodapati <41941282+pujita25@users.noreply.github.com>
2023-03-18 09:48:40 -0400

    Add files via upload

---
+++ ReferenceTypesInJava/src/PrintStringChar.java
@@ -0,0 +1,12 @@
+
+public class PrintStringChar {
+
+	public static void main(String[] args) {
+		String str = "This is a very long text and very annoying";
+		for (int i = 0; i < str.length(); i++) {
+			System.out.println(str.charAt(i));
+		}
+
+	}
+
+}
